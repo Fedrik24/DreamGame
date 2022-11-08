26d863707f40c6adb1b3024c000a3a354002c9e1
Fedrik24 <fedrikarya24@gmail.com>
2022-11-08 20:03:12 +0700

    WIP : Basic Combat System
    - Add Combat Target
    - Add FighterClass

---
+++ Assets/Script/Combat/Fighter.cs
@@ -0,0 +1,41 @@
+﻿using Dreams.Movement;
+using UnityEngine;
+
+namespace Dreams.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        [SerializeField] private Mover move;
+        [SerializeField] private float weaponRange = 2f; // Can use ScriptableObject in the future...
+        
+        private Transform target;
+        private void Update()
+        {
+            if (target == null) return;
+            
+            if (!GetIsInRange())
+            {
+                move.MoveTo(target.position);
+            }
+            else
+            {
+                move.Stop();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+
+        public void CancelAttack()
+        {
+            target = null;
+        }
+    }
+}
