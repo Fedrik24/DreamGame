b074aa88e6b5a3c68bc96dc58d3a4b46bde3a645
Fedrik24 <fedrikarya24@gmail.com>
2022-11-13 13:44:05 +0700

    Feat : Add asset
    - Fix : Character bug animation
    - Feat : Add Health to each character
    - Fix : Remove Namespace depedency

---
+++ Assets/Script/Combat/Health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dreams.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private float healthPoints = 100f;
+        [SerializeField] private Animator animator; // Bisa di jadiin 1 Script untuk semua animator
+
+        private bool isDead = false;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+        
+        private void Die()
+        {
+            if(isDead) return;
+            animator.SetTrigger("die");
+            isDead = true;
+        }
+    }
+}
