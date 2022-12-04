64275a1c625e3f262e230fad841eab78e7e4a4fa
Fedrik24 <fedrikarya24@gmail.com>
2022-12-04 12:20:36 +0700

    Feat : Add Cinemachine
    - Chore : First Moment
    - WIP : Disable Control

---
+++ Assets/Script/Cinematic/CinematicControlRemover.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace Dreams.Cinematic
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        private void DisableControl()
+        {
+            Debug.Log($"WIP DisableControl");
+        }
+
+        private void EnableControl()
+        {
+            Debug.Log($"WIP EnableControl");
+        }
+    }
+}
